6b26edb7b785b16dd4a86357bcbcf99b3d018e2a
William Viker <william.viker@gmail.com>
2019-09-25 16:21:26 +0200

    optoma rewrite

--- index.js
+++ index.js
@@ -25,26 +25,17 @@ instance.prototype.updateConfig = function(config) {
 	self.init_tcp();
 };
 
-instance.prototype.init = function() {
-	var self = this;
-
-	debug = self.debug;
-	log = self.log;
-
-	self.states = {}
-	self.init_feedbacks()
-
-	self.init_tcp();
-};
-
 instance.prototype.incomingData = function(data) {
 	var self = this;
-	debug(data);
+	debug("OPTOMA IN", data);
+	if (data.match("Projector")) {
+		self.status(self.STATUS_OK,'OK');
+	}
 
+/*
 	// Match part of the copyright response from unit when a connection is made.
 	if (self.login === false && data.match("Extron Electronics")) {
 		self.status(self.STATUS_WARNING,'Logging in');
-		self.socket.write("\x1B3CV"+ "\r"); // Set Verbose mode to 3
 		self.socket.write("2I"+ "\n"); // Query model description
 	}
 
@@ -71,24 +62,20 @@ instance.prototype.incomingData = function(data) {
 		var beat_period = 180; // Seconds
 		self.heartbeat_interval = setInterval(heartbeat, beat_period * 1000);
 	}
-	// Match recording state change expected response from unit.
-	if (self.login === true && data.match(/RcdrY\d+/)) {
-		self.states['record_bg'] = parseInt(data.match(/RcdrY(\d+)/)[1]);
-		self.checkFeedbacks('record_bg');
-		debug("recording change");
-		}
-	// Match stream state change expected response from unit.
-	if (self.login === true && data.match(/RtmpE1\*\d+/)) {
-		self.states['rtmp_push_bg'] = parseInt(data.match(/RtmpE1\*(\d+)/)[1]);
-		self.checkFeedbacks('rtmp_push_bg');
-		debug("stream change");
-	}
-
 	else {
 		debug("data nologin", data);
-	}
+	}*/
+
 };
 
+instance.prototype.init = function() {
+	var self = this;
+
+	debug = self.debug;
+	log = self.log;
+
+	self.init_tcp();
+};
 
 instance.prototype.init_tcp = function() {
 	var self = this;
@@ -112,11 +99,16 @@ instance.prototype.init_tcp = function() {
 		self.socket.on('error', function (err) {
 			debug("Network error", err);
 			self.log('error',"Network error: " + err.message);
-			self.login = false;
 		});
 
 		self.socket.on('connect', function () {
 			debug("Connected");
+			self.login = false;
+		});
+
+		self.socket.on('error', function (err) {
+			debug("Network error", err);
+			self.log('error',"Network error: " + err.message);
 			self.login = false;
 		});
 
@@ -140,16 +132,6 @@ instance.prototype.init_tcp = function() {
 	}
 };
 
-instance.prototype.CHOICES_RECORD = [
-	{ label: 'STOP', id: '0' },
-	{ label: 'START', id: '1' },
-	{ label: 'PAUSE', id: '2' }
-]
-
-instance.prototype.CHOICES_ONOFF = [
-	{ label: 'OFF', id: '0' },
-	{ label: 'ON', id: '1'}
-]
 
 // Return config fields for web config
 instance.prototype.config_fields = function () {
@@ -161,14 +143,14 @@ instance.prototype.config_fields = function () {
 			id: 'info',
 			width: 12,
 			label: 'Information',
-			value: 'This will establish a telnet connection to the SMP 111'
+			value: 'This will establish a tcp connection to the projector'
 		},
 		{
 			type: 'textinput',
 			id: 'host',
-			label: 'SMP IP address',
+			label: 'Projector IP address',
 			width: 12,
-			default: '192.168.254.254',
+			default: '192.168.0.1',
 			regex: self.REGEX_IP
 		}
 	]
@@ -177,137 +159,25 @@ instance.prototype.config_fields = function () {
 // When module gets deleted
 instance.prototype.destroy = function() {
 	var self = this;
-	clearInterval (self.heartbeat_interval); //Stop Heartbeat
+	//clearInterval (self.heartbeat_interval); //Stop Heartbeat
 
 	if (self.socket !== undefined) {
 		self.socket.destroy();
 	}
 
-	self.states = {}
-
 	debug("destroy", self.id);
 };
-
-instance.prototype.init_feedbacks = function () {
-	var self = this
-	var feedbacks = {}
-
-	feedbacks['record_bg'] = {
-		label: 'Change colors for Record state',
-		description: 'If Record state specified is in use, change colors of the bank',
-		options: [
-			{
-				type: 'colorpicker',
-				label: 'Foreground color',
-				id: 'fg',
-				default: self.rgb(255, 255, 255)
-			},
-			{
-				type: 'colorpicker',
-				label: 'Background color',
-				id: 'bg',
-				default: self.rgb(0, 255, 0)
-			},
-			{
-				type: 'dropdown',
-				label: 'record',
-				id: 'record',
-				default: 1,
-				choices: self.CHOICES_RECORD
-			}
-		]
-	}
-
-	feedbacks['rtmp_push_bg'] = {
-		label: 'Change colors for RTMP Push Options',
-		description: 'If RTMP Push Stream in use, change colors of the bank',
-		options: [
-			{
-				type: 'colorpicker',
-				label: 'Foreground color',
-				id: 'fg',
-				default: self.rgb(255, 255, 255)
-			},
-			{
-				type: 'colorpicker',
-				label: 'Background color',
-				id: 'bg',
-				default: self.rgb(0, 255, 0)
-			},
-			{
-				type: 'dropdown',
-				label: 'rtmp_push',
-				id: 'rtmp_push',
-				default: 1,
-				choices: self.CHOICES_ONOFF
-			}
-		]
-	}
-	self.setFeedbackDefinitions(feedbacks)
-}
-
-instance.prototype.feedback = function (feedback, bank) {
-	var self = this
-
-	if (feedback.type === 'record_bg') {
-		if (self.states['record_bg'] === parseInt(feedback.options.record)) {
-			return { color: feedback.options.fg, bgcolor: feedback.options.bg }
-		}
-	}
-
-	if (feedback.type === 'rtmp_push_bg') {
-		if (self.states['rtmp_push_bg'] === parseInt(feedback.options.rtmp_push)) {
-			return { color: feedback.options.fg, bgcolor: feedback.options.bg }
-		}
-	}
-
-	return {}
-}
 
 instance.prototype.actions = function(system) {
 	var self = this;
 	var actions = {
-		'start_rec': {
-			label: 'Start recording',
-			options: [{
-					label: 'start record',
-					id: 'start_rec',
-			}]
+		'power_off': {
+			label: 'Power off',
+			options: []
 		},
-		'stop_rec': {
-			label: 'Stop recording',
-			options: [{
-					label: 'stop record',
-					id: 'stop_rec',
-			}]
-		},
-		'pause_rec': {
-			label: 'Pause recording',
-			options: [{
-					label: 'pause record',
-					id: 'pause_rec',
-			}]
-		},
-		'mark_rec': {
-			label: 'Mark recording',
-			options: [{
-					label: 'mark record',
-					id: 'mark_rec',
-			}]
-		},
-		'rtmp_off': {
-			label: 'RTMP Off',
-			options: [{
-					label: 'rtmp off',
-					id: 'rtmp_off',
-			}]
-		},
-		'rtmp_on': {
-			label: 'RTMP On',
-			options: [{
-					label: 'rtmp on',
-					id: 'rtmp_on',
-			}]
+		'power_on': {
+			label: 'Power on',
+			options: []
 		}
 	};
 
@@ -322,42 +192,19 @@ instance.prototype.action = function(action) {
 	var cmd;
 
 	switch (id) {
-		case 'start_rec':
-			cmd = "\x1BY1RCDR";
+		case 'power_off':
+			cmd = "\x7E\x30\x30\x30\x30\x20\x30\x0D";
 			break;
-
-		case 'stop_rec':
-			cmd = "\x1BY0RCDR";
+		case 'power_on':
+			cmd = "\x7E\x30\x30\x30\x30\x20\x31\x0D";
 			break;
-
-		case 'pause_rec':
-			cmd = "\x1BY2RCDR";
-			break;
-
-		case 'mark_rec':
-			cmd = "\x1BBRCDR";
-			break;
-
-		case 'rtmp_off':
-			cmd = "\x1BE1*0RTMP";
-			break;
-
-		case 'rtmp_on':
-			cmd = "\x1BE1*1RTMP";
-			break;
-	}
-
-	if (cmd !== undefined) {
-			if (self.tcp !== undefined) {
-					debug('sending ', cmd, "to", self.tcp.host);
-					self.tcp.send(cmd);
-			}
 	}
 
 	if (cmd !== undefined) {
 
 		if (self.socket !== undefined && self.socket.connected) {
-			self.socket.write(cmd+"\n");
+			console.log(new Buffer(cmd));
+			self.socket.write(cmd);
 		} else {
 			debug('Socket not connected :(');
 		}
